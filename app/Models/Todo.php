5666e9190a2b3ad300564286de393b7e31c9b307
Đoàn Xuân Sinh Globo Dev <82463517+Sinhdoan2000@users.noreply.github.com>
2022-07-19 13:37:02 +0700

    Update Todo.php

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    protected $table = 'job';
+    protected $table = 'Todo';
     protected $fillable = ['TITLE', 'STATUS'];
 }
